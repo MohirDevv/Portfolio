228750e2c2b44880db9798b5a7f12732ff6defb4
Mohir <bahtisat442@gmail.com>
2024-01-22 19:41:40 +0500

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container px-[20px] py-[10px] md:px-[50px] md:py-[15px] flex items-center justify-between">
+    <h1 class="text-[26px] font-bold text-semiBlack cursor-pointer flex items-center justify-center font-mono">Mohir.<p class="text-lightGreen font-mono text-[25px] font-bold">dev</p></h1>
+    <div class="menu gap-5 text-semiBlack hidden md:flex">
+      <p class="text-[20px] font-medium cursor-pointer">Home</p>
+      <p class="text-[20px] font-medium cursor-pointer">About</p>
+      <p class="text-[20px] font-medium cursor-pointer">Projects</p>
+      <p class="text-[20px] font-medium cursor-pointer">Contacts</p>
+    </div>
+    <div class="mobileMenu flex items-center md:hidden">
+      <i class="fas fa-bars text-[20px] text-semiBlack cursor-pointer"></i>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .container{
+    box-shadow: 0 5px 20px rgba(0,0,0,.09)
+  }
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx,vue}",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      'semiBlack': '#2d2e32',
+      'lightGreen': '#6aaa64',
+    },
+    fontFamily: {
+      sans: ["Rubik", "sans-serif"],
+      mono: ["Roboto Mono", "monospace"],
+    },
+  },
+  plugins: [],
+}
+
